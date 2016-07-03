1d28fcc0c23f1dfb2cc9897c8fce3efbce402079
la-mari <karima19@gmail.com>
2016-07-03 13:14:59 -0700

    working redrawing of image

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function setup() {
 	img.loadPixels();
 	background(0);
 	stroke(255, 0, 0);
+	strokeWeight(2);
 	//slit postitioned middle of screen
 	center = width/2;
 }
@@ -22,10 +23,33 @@ function setup() {
 function draw(){
 	background(0);
 	var mx = constrain(mouseX, center-s/2, center+s/2);
-
+	//map mx from center-s/2 to +s/2 to s to 0
 	var x = map(mx, center-s/2, center+s/2, s, 0);
+	//make integer
+	x = floor(x);
+
+
+	//get color at y position from 0 to height, set to c
+	for (var y=0; y<height; y++){
+		var c = img.get(x, y);
+		//position on top moving across the screen setting to color c
+		//creates new column of pixels
+		set(draw_postion_x, y, c);
+	}
+	updatePixels();
+
 	//thumbnail
 	image(img, mx, height-s/2); //position at bottom of screen
 	line(width/2, height-s, width/2, height);
+
+	//line connecting sketches
+	line(draw_postion_x, s, width/2, height-s);
+
+	//shift draw postion after each column drawn
+	draw_postion_x++;
+
+	if (draw_postion_x >= width){
+		draw_postion_x = 0;
+	}
 	
 }
